75b5a0a6e6fa0a9a31f4b585a70b255afe48ffde
Rob <rob.mcvey@announcemedia.com>
2012-08-28 15:06:22 -0500

    Added base64 encoding/decoding

--- src/winjq.js
+++ src/winjq.js
@@ -438,6 +438,86 @@ var $ = (function(){
 	};
 
 	$.extend({
+		keyStr: "ABCDEFGHIJKLMNOP" +
+		               "QRSTUVWXYZabcdef" +
+		               "ghijklmnopqrstuv" +
+		               "wxyz0123456789+/" +
+		               "=",
+		encode64: function(input) {
+		     input = escape(input);
+		     var output = "";
+		     var chr1, chr2, chr3 = "";
+		     var enc1, enc2, enc3, enc4 = "";
+		     var i = 0;
+
+		     do {
+		        chr1 = input.charCodeAt(i++);
+		        chr2 = input.charCodeAt(i++);
+		        chr3 = input.charCodeAt(i++);
+
+		        enc1 = chr1 >> 2;
+		        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+		        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+		        enc4 = chr3 & 63;
+
+		        if (isNaN(chr2)) {
+		           enc3 = enc4 = 64;
+		        } else if (isNaN(chr3)) {
+		           enc4 = 64;
+		        }
+
+		        output = output +
+		           this.keyStr.charAt(enc1) +
+		           this.keyStr.charAt(enc2) +
+		           this.keyStr.charAt(enc3) +
+		           this.keyStr.charAt(enc4);
+		        chr1 = chr2 = chr3 = "";
+		        enc1 = enc2 = enc3 = enc4 = "";
+		     } while (i < input.length);
+
+		     return output;
+		},
+		decode64: function(input) {
+		     var output = "";
+		     var chr1, chr2, chr3 = "";
+		     var enc1, enc2, enc3, enc4 = "";
+		     var i = 0;
+
+		     // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+		     var base64test = /[^A-Za-z0-9\+\/\=]/g;
+		     if (base64test.exec(input)) {
+		        alert("There were invalid base64 characters in the input text.\n" +
+		              "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+		              "Expect errors in decoding.");
+		     }
+		     input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+		     do {
+		        enc1 = this.keyStr.indexOf(input.charAt(i++));
+		        enc2 = this.keyStr.indexOf(input.charAt(i++));
+		        enc3 = this.keyStr.indexOf(input.charAt(i++));
+		        enc4 = this.keyStr.indexOf(input.charAt(i++));
+
+		        chr1 = (enc1 << 2) | (enc2 >> 4);
+		        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+		        chr3 = ((enc3 & 3) << 6) | enc4;
+
+		        output = output + String.fromCharCode(chr1);
+
+		        if (enc3 != 64) {
+		           output = output + String.fromCharCode(chr2);
+		        }
+		        if (enc4 != 64) {
+		           output = output + String.fromCharCode(chr3);
+		        }
+
+		        chr1 = chr2 = chr3 = "";
+		        enc1 = enc2 = enc3 = enc4 = "";
+
+		     } while (i < input.length);
+
+		     return unescape(output);
+		},
 		each: function( object, callback ){
 			return $.fn.each( object, callback );
 		},
@@ -459,7 +539,7 @@ var $ = (function(){
 			var options = {
 				method: 'GET',
 				url: r_url,
-				success: r_success || $.fn.noop,
+				success: r_success,
 				error: r_error || $.fn.noop
 			};
 			$.fn.ajax( options );
@@ -474,6 +554,7 @@ var $ = (function(){
 					return 'error: '+e.message;
 				}
 			});
+			return this;
 		},
 		nodeName: function( elem, name ) {
 			return elem.nodeName && elem.nodeName.toUpperCase() === name.toUpperCase();
@@ -487,6 +568,7 @@ var $ = (function(){
 				script.type = 'text/javascript'; script.onload = cb || function(e){};
 				(d.getElementsByTagName('head')[0] || d.getElementByTagName('body')[0]).appendChild(script);
 			})(document, script, callback);
+			return this;
 		},
 		fireReady: function(){
 			if( $.fn.domLoaded === true ){
