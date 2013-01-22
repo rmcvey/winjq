4136b3d307a82e41c2c5f3536ac86f1ea216664b
Rob <mcvey.rob@gmail.com>
2013-01-22 13:56:12 -0600

    Update src/plugins/page-animate.js
    added basic usage info on pageAnimate

--- src/plugins/page-animate.js
+++ src/plugins/page-animate.js
@@ -1,41 +1,46 @@
-;(function($){
-	$.fn.pageAnimate = function(dir, options, fn_success, fn_error){
-		var element = $(this).get(0),
+/**
+ * 	- BASIC USAGE
+ *  $('#page_content_selector').enterPage({ top: "0px", left: "200px", rtlflip: true });
+ *  $('#page_content_selector').exitPage({ top: "0px", left: "200px", rtlflip: true });
+ */
+;(function ($) {
+    $.fn.pageAnimate = function (dir, options, fn_success, fn_error) {
+        var element = $(this).get(0),
 			$this = this,
-			success = function(elem){
-				var context = $this[0];
-				return fn_success.apply(context, [context]);
+			success = function (elem) {
+			    var context = $(this).get(0);
+			    return (fn_success || function () { }).apply(context, [context]);
 			},
-			error = function(elem){
-				var context = $this[0];
-				return fn_error.apply(context, [context]);
+			error = function (elem) {
+			    var context = $(this).get(0);
+			    return fn_error.apply(context, [context]);
 			},
 			params = options || null,
 			direction = dir || 'enterPage';
-		
-		WinJS.UI.Animation[direction](element, params).done(
+
+        WinJS.UI.Animation[direction](element, params).done(
 			success, error
 		);
-		return this;
-	};
-	$.fn.enterPage = function(options, fn_success, fn_error){
-		if(this._util.is.callable(options)){
-			fn_success = options;
-			fn_error = fn_success;
-			options = null;
-		} else if(!options){
-			options = null;
-		}
-		return $(this).pageAnimate('enterPage', options, fn_success, fn_error);
-	};
-	$.fn.exitPage = function(options, fn_success, fn_error){
-		if(this._util.is.callable(options)){
-			fn_success = options;
-			fn_error = fn_success;
-			options = null;
-		} else if(!options){
-			options = null;
-		}
-		return $(this).pageAnimate('exitPage', options, fn_success, fn_error);
-	};
+        return this;
+    };
+    $.fn.enterPage = function (options, fn_success, fn_error) {
+        if ($.isFunction(options)) {
+            fn_success = options;
+            fn_error = fn_success;
+            options = null;
+        } else if (!options) {
+            options = null;
+        }
+        return $(this).pageAnimate('enterPage', options, fn_success, fn_error);
+    };
+    $.fn.exitPage = function (options, fn_success, fn_error) {
+        if ($.isFunction(options)) {
+            fn_success = options;
+            fn_error = fn_success;
+            options = null;
+        } else if (!options) {
+            options = null;
+        }
+        return $(this).pageAnimate('exitPage', options, fn_success, fn_error);
+    };
 })(window.$);
